0b3ed1a3e87eeecb27f41cb5582e4d61f9e9631a
Valera-123 <149613398+Valera-123@users.noreply.github.com>
2025-02-15 20:55:51 +0300

    Added entry point

---
+++ Application.cs
@@ -0,0 +1,10 @@
+﻿namespace _202.Larin.wdad
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I’m Larin, and I’m not a monkey");
+        }
+    }
+}
